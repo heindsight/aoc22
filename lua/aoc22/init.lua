80db887cdcc58953279438ac2e02cbb2002e8650
Heinrich Kruger <heindsight@kruger.dev>
2022-12-06 01:07:25 +0000

    Prevent command completion of (or trying to load) anything outside the solutions directory

--- lua/aoc22/init.lua
+++ lua/aoc22/init.lua
@@ -11,6 +11,12 @@ local P = {}
 
 -- Argument completion for puzzle solutions
 function P.complete_solutions(arg)
+    if arg:match("[^%w_]") then
+        -- Only expand alphanumeric characters
+        -- (we don't want to allow injection of glob wildcard characters here).
+        return nil
+    end
+
     local soln_dir = vim.fs.dirname(script_path()) .. "/solutions/"
     local available_solutions = vim.fn.glob(soln_dir .. arg .. "*.lua", true, true)
     return vim.tbl_map(
@@ -21,6 +27,11 @@ end
 
 -- Load a solution and run it on the current buffer. Open the output buffer in a new window.
 function P.solve_puzzle(opts)
+    if opts.args:match("[^%w_]") then
+        -- Only allow alphanumeric characters and underscores in puzzle name
+        error(string.format("Invalid puzzle name: %q", opts.args))
+    end
+
     local solution = require("aoc22.solutions." .. opts.args)
     local in_buffer = vim.api.nvim_get_current_buf()
     local out_buffer = solution.solve(in_buffer)
